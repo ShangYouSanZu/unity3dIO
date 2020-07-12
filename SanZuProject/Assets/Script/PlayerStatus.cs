b265dcc85e720ed917cd3c8143280298eff36715
YaRen <c.yejiajun@foxmail.com>
2020-07-12 20:29:04 +0800

    初始环境，包含两个插件

---
+++ SanZuProject/Assets/Script/PlayerStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int HP = 100; //血量
+    public float movementSpeed = 5; //移动速度
+
+
+}
